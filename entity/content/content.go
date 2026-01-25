d31cd69d9a219fe6ca986bf57d76948846043ffc
osamikoyo <v.pylayev@mail.ru>
2026-01-25 08:10:13 +0500

    update content

--- entity/content/content.go
+++ entity/content/content.go
@@ -1,15 +1,11 @@
 package content
 
 type Content struct{
-	LastID string
 	RIndentifer string
-	Actions int
 }
 
-func NewContent(lastID, rID string) *Content {
+func NewContent(rID string) *Content {
 	return &Content{
-		LastID: lastID,
 		RIndentifer: rID,
-		Actions: 0,
 	}
 }
