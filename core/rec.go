552357ec54e30a0c94d493efa78fe13085389ba1
osamikoyo <v.pylayev@mail.ru>
2026-01-25 08:10:02 +0500

    add rec sort type

---
+++ core/rec.go
@@ -0,0 +1,12 @@
+package core
+
+type rec struct{
+	Value int
+	Rid string
+}
+
+type Recs []rec
+
+func (a Recs) Len() int           { return len(a) }
+func (a Recs) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a Recs) Less(i, j int) bool { return a[i].Value < a[j].Value }
